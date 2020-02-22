2c06ba7479dd51395391f14bf9209136f96db93d
youlei <yooleiyou@cityocean.com>
2020-02-22 20:31:32 +0800

    编译报错

--- src/app/components/business-type/business-type.component.ts
+++ src/app/components/business-type/business-type.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./business-type.component.scss'],
 })
 export class BusinessTypeComponent implements OnInit {
-  @Input() title = {};
+  @Input() title = {title1:'',title2:''};
   @Input() status;
   @Input() type;
   constructor() { }
