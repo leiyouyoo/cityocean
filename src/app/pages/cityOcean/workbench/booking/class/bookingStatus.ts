f6dba9d2d56eb6beb8d3fc96d87aa4421f5fd481
youlei <yooleiyou@cityocean.com>
2020-02-25 17:44:30 +0800

    booking提交状态流程

---
+++ src/app/pages/cityOcean/workbench/booking/class/bookingStatus.ts
@@ -0,0 +1,10 @@
+export enum bookingStatus{
+    Draft=0,
+    Cancelled=1,
+    Submitted=2,
+    Booked=3,
+    WaitingForPricing=4,
+    WaitingForBuyer=5,
+    WaitingForSellerr=6,
+    ConfirmCancelled=7
+}
