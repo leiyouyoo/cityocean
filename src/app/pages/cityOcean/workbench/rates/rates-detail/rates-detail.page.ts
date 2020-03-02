2fe4ddd5c5bbe232f1433f61523c0b197f7e241a
youlei <yooleiyou@cityocean.com>
2020-03-02 11:51:03 +0800

    rates详情页

--- src/app/pages/cityOcean/workbench/rates/rates-detail/rates-detail.page.ts
+++ src/app/pages/cityOcean/workbench/rates/rates-detail/rates-detail.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { RatesService } from '../rates.service';
+import * as moment from 'moment';
+import { CityOceanService } from '../../../city-ocean.service';
 
 @Component({
   selector: 'app-rates-detail',
@@ -12,25 +14,34 @@ export class RatesDetailPage implements OnInit {
   AdditionalData: any;
 
   constructor(private nav:NavController,
-    private ratesService:RatesService) { }
+    private ratesService:RatesService,
+    private cityOceanService:CityOceanService) { }
 
   ngOnInit() {
     this.ratesDetail = this.ratesService.ratesDetail;
+    this.AdditionalData= this.ratesService.ratesDetail.chargesGroups[0];
     console.log(this.ratesDetail)
   }
   goback(){
     // this.nav.navigateForward(['/cityOcean/workbench/rates']);
     window.history.back();
   }
+  getTime(time) {
+    return moment(time).format("YYYY/MM/DD");
+  }
   segmentChanged($event){
-    console.log($event)
+    this.onTableSelected($event)
   }
   onTableSelected(selected) {
     this.AdditionalData = null;
-    // this.AdditionalTableData.chargesGroups.forEach((r) => {
-    //   if (r.freightCharges.containerCode === selected) {
-    //     this.AdditionalData = r;
-    //   }
-    // });
+    this.ratesDetail.chargesGroups.forEach((r) => {
+      if (r.freightCharges.containerCode === selected.detail.value) {
+        this.AdditionalData = r;
+      }
+    });
+  }
+  // 客服
+  chatWithCustomer() {
+    this.cityOceanService.chatWithCustomerService();
   }
 }
