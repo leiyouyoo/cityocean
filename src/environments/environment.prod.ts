72c76702da4d02666ddf5199150849d8ee710e3e
penglin <519645581@qq.com>
2020-03-13 17:30:14 +0800

    修改版本及参数

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,19 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`
+
 export const environment = {
-  // SERVER_URL: `http://csp.cityocean.com:20200`, // 远端服务器地址（必需的）
-  // SignalR_Url: `http://csp.cityocean.com:20201`, // SignalR_SERVER_URL服务器地址（必需的）
-  SERVER_URL: `http://112.95.173.231:8000`, // 远端服务器地址（必需的）
-  SignalR_Url: `http://112.95.173.231:8002`, // SignalR_SERVER_URL服务器地址（必需的）
-
-  //appBaseUrl: `http://wechat.cityocean.com:8994`, // APP 地址
-  appBaseUrl: `http://wx2.cityocean.com:8970`, // APP 地址
-  MOCK_URL: '',
-  StoreUrl: 'http://112.95.173.231:8000',
-  uploadUrl: `http://192.168.1.5:8000/Storage/File/Upload`,
-  ImImageUrl: `http://112.95.173.230:8002`,
-  // activity cityoceanId
+  MOCK_URL: `http://api.cityocean.com:20001`,
+  SERVER_URL: `http://api.cityocean.com:20001`, // 远端服务器地址（必需的）
+  SignalR_Url: `http://192.168.1.6:8002`, // SignalR_SERVER_URL服务器地址（必需的）
+  appBaseUrl: `http://localhost:8100/`, // APP 地址
+  StoreUrl: 'http://api.cityocean.com:20001',
+  uploadUrl: `http://api.cityocean.com:20001/Storage/File/Upload`,
+  ImImageUrl: `http://api.cityocean.com:20001`,
   cityoceanId: 2,
-
-  production: true,
+  production: false,
   useHash: true,
   hmr: false,
 };
