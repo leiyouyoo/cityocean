30ad01c039f4926422c209b528e16fceb862cf90
penglin <519645581@qq.com>
2020-03-14 16:40:33 +0800

    11

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,13 +3,13 @@
 // The list of file replacements can be found in `angular.json`
 
 export const environment = {
-  MOCK_URL: `http://api.cityocean.com:20001`,
-  SERVER_URL: `http://api.cityocean.com:20001`, // 远端服务器地址（必需的）
-  SignalR_Url: `http://192.168.1.6:8002`, // SignalR_SERVER_URL服务器地址（必需的）
-  appBaseUrl: `http://localhost:8100/`, // APP 地址
-  StoreUrl: 'http://api.cityocean.com:20001',
-  uploadUrl: `http://api.cityocean.com:20001/Storage/File/Upload`,
-  ImImageUrl: `http://api.cityocean.com:20001`,
+  MOCK_URL: `https://api.cityocean.com:20001`,
+  SERVER_URL: `https://api.cityocean.com:20001`, // 远端服务器地址（必需的）
+  SignalR_Url: `https://192.168.1.6:8002`, // SignalR_SERVER_URL服务器地址（必需的）
+  appBaseUrl: `https://localhost:8100/`, // APP 地址
+  StoreUrl: 'https://api.cityocean.com:20001',
+  uploadUrl: `https://api.cityocean.com:20001/Storage/File/Upload`,
+  ImImageUrl: `https://api.cityocean.com:20001`,
   cityoceanId: 2,
   production: false,
   useHash: true,
