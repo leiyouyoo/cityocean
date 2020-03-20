35c0d9a01e675ecf58876a461e4606f61fe51547
penglin <519645581@qq.com>
2020-03-20 15:34:04 +0800

    ios注册

---
+++ src/app/pages/sso/register/register.page.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Helper } from '@shared/helper';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+  public RegisterForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    public helper: Helper,
+    public nav: NavController,
+    public activeRoute: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    this.initData();
+  }
+
+  /**
+   * @title 初始化数据
+   * @desc 初始化登录表单内容
+   */
+  initData() {
+    this.RegisterForm = this.formBuilder.group({
+      userName: [null, Validators.compose([Validators.required])],
+      nickName: [null, Validators.compose([Validators.required])],
+      userPhone: [null, Validators.compose([Validators.required])],
+      phoneNum: [null, Validators.compose([Validators.required])],
+      userPwd: [null, Validators.compose([Validators.required])],
+    });
+  }
+
+  /**
+   * @title 用户点击注册
+   * @desc 用户注册接口调用
+   */
+  onRegister(event) {
+    // this.helper.showLoading('账号申请中');
+    // this.appService
+    //   .post('/api/user/UserRegister', {
+    //     UserName: event.userName,
+    //     NickName: event.nickName,
+    //     UserPhone: event.userPhone,
+    //     Password: event.userPwd,
+    //   })
+    //   .subscribe(
+    //     (res: any) => {
+    //       this.helper.hideLoading();
+    //       if (res.Code == 200) {
+    //         this.helper.toast('注册成功!立即登录吧');
+    //         this.router.navigate(['/'], {
+    //           queryParams: { UserName: event.userName, Password: event.userPwd },
+    //         });
+    //       }
+    //     },
+    //     (err) => {
+    //       this.helper.hideLoading();
+    //     },
+    //   );
+  }
+
+  sendSms() {
+    this.helper.toast('手机号码格式不正确');
+  }
+}
--- src/app/pages/sso/sso.route.module.ts
+++ src/app/pages/sso/sso.route.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterPage } from './register/register.page';
 
 const routes: Routes = [
-  { path: "", component: LoginComponent }
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterPage },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forChild(routes),
-  ],
-  exports: [
-    RouterModule
-  ],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class SsoRouteModule { }
+export class SsoRouteModule {}
